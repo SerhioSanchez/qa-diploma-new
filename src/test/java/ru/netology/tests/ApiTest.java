ef5cfeac486532bb8af61a67c71662a8cad02fe0
SerhioSanchez <132932149+SerhioSanchez@users.noreply.github.com>
2026-03-18 21:34:50 +0300

    Update ApiTest.java

--- src/test/java/ru/netology/tests/ApiTest.java
+++ src/test/java/ru/netology/tests/ApiTest.java
@@ -8,41 +8,33 @@ import static ru.netology.data.ApiHelper.creditForm;
 import static ru.netology.data.BankCardDataHelper.getCardInValidValuesNextMonthYear;
 import static ru.netology.data.BankCardDataHelper.getCardValidValuesCurrentMonthYear;
 
-/**
- * API-тесты (через ApiHelper).
- * Минимальный набор для диплома: 2 статуса (APPROVED/DECLINED) × 2 формы (Оплата/Кредит).
- */
 public class ApiTest {
 
-    // Оплата по карте -> APPROVED
     @Test
-    void cardPayApproved() {
-        var card = getCardValidValuesCurrentMonthYear();
-        var status = buyForm(card);
+    void cardBuyApproved() {
+        var approvedCard = getCardValidValuesCurrentMonthYear();
+        var status = buyForm(approvedCard);
         assertTrue(status.contains("APPROVED"));
     }
 
-    // Оплата по карте -> DECLINED
     @Test
-    void cardPayDeclined() {
-        var card = getCardInValidValuesNextMonthYear();
-        var status = buyForm(card);
+    void cardBuyDeclined() {
+        var declinedCard = getCardInValidValuesNextMonthYear();
+        var status = buyForm(declinedCard);
         assertTrue(status.contains("DECLINED"));
     }
 
-    // Кредит по данным карты -> APPROVED
     @Test
-    void creditPayApproved() {
-        var card = getCardValidValuesCurrentMonthYear();
-        var status = creditForm(card);
+    void cardCreditApproved() {
+        var approvedCard = getCardValidValuesCurrentMonthYear();
+        var status = creditForm(approvedCard);
         assertTrue(status.contains("APPROVED"));
     }
 
-    // Кредит по данным карты -> DECLINED
     @Test
-    void creditPayDeclined() {
-        var card = getCardInValidValuesNextMonthYear();
-        var status = creditForm(card);
+    void cardCreditDeclined() {
+        var declinedCard = getCardInValidValuesNextMonthYear();
+        var status = creditForm(declinedCard);
         assertTrue(status.contains("DECLINED"));
     }
 }
